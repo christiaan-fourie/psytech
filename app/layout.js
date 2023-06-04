ae7f17d7d72b0f5bcfd481459a4d322becacc4e2
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-05 00:07:32 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 
-
+import StyledCursor from '@/components/Cursor/Cursor'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <StyledCursor />
         {children}
       </body>
     </html>
