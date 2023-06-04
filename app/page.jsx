7db50efb821bbac8aa6c0d61ca1a2227d3b74ffe
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-04 21:30:05 +0200

    Install Tailwind
    Installing tailwind
    Building a header for mobile and desktop.

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+
+import { FaGithub } from 'react-icons/fa'
+
+export default function Home() {
+  return (
+    <main className='flex flex-col justify-center items-center'>   
+      Home
+    </main>
+  )
+}
