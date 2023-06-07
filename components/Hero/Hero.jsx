0e5e85bdd525600c67ab62c3cafd700bb2f8efe0
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-07 22:21:16 +0200

    Add a matrix backdrop effect on the hero page.

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,19 +1,19 @@
 'use client'
 
+import MatrixBackgroundComponent from "./MatricBackgroundComponent"
+
+
 export default function Hero() {
 
 
 
   return (
     <div>
-        <div className='text-left p-24 md:text-center mt-[20vh] z-0 min-h-screen'>
-            <h4>We Do Magic</h4>
-            <h1 className='text-6xl'>Road to <span className='text-blue-500'>Success</span></h1>
+        <div className='p-12 md:p-24 text-center mt-[20vh] z-0'>
+            <h4>We do the Magic</h4>
+            <h1 className='text-6xl'>You focus on <span className='text-blue-500'>Success</span></h1>
         </div>
-        <div className='text-left p-24 md:text-center mt-[20vh] z-0 min-h-screen'>
-            <h4>We Do Magic</h4>
-            <h1 className='text-6xl'>Road to <span className='text-blue-500'>Success</span></h1>
-        </div>
+        <MatrixBackgroundComponent />
     </div>
     
   )
