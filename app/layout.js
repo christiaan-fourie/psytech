be7233addc7494c54608c99924fda71467dfc332
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-06-07 22:19:47 +0200

    Scroll To Top Button

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 
 import StyledCursor from '@/components/Cursor/Cursor'
+import ScrollToTop from '@/components/ScrollToTop/ScrollToTop'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <StyledCursor />
         {children}
+        <ScrollToTop />
       </body>
     </html>
   )
