42c2ac0e2f28a9df7a5334224969cf044256a6f3
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-07-27 10:43:35 +0200

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -10,12 +10,8 @@ export default function Hero() {
 
   return (
     <div>
-        <div className='p-12 md:p-24 text-center mt-[20vh] z-0'>
-            <h4>We do the Magic</h4>
-            <h1 className='text-6xl'>You focus on <span className='text-blue-500'>Success</span></h1>
-        </div>
         <div className='flex items-center'>
-          <Image alt='Psytech Software Core' className='animate-pulse' width={500} height={250} src='/technology.jpg'></Image>
+          <Image alt='Psytech Software Core' className='animate-pulse' width={500} height={250} src='/technology.png'></Image>
           <div>
             <h1 className='text-8xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-red-500'>Core Focus</h1>
             <p>
