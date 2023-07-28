8487a619d30e3b2aaceac10ecae4a82ccc525408
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-07-28 11:12:54 +0200

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
   return (
     <div>
         <div className='flex items-center pt-[160px]'>
-          <div className='flex mx-12 justify-center items-center'>
+          <div className='flex flex-col md:flex-row mx-12 justify-center items-center'>
             <Image alt='Psytech Software Core' className='animate-pulse' width={500} height={250} src='/technology.png'></Image>
             <div>
               <h1 className='text-4xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-red-500'>Security is at our Core</h1>
