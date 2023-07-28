45ed2d0994f94c8c052d157a48fe182fd3484720
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-07-28 11:17:17 +0200

    Update PriceCards.jsx

--- components/PriceCards/PriceCards.jsx
+++ components/PriceCards/PriceCards.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const PriceCards = () => {
   return (
-    <div className='flex flex-col gap-4 md:flex-row justify-around md:p-8 mx-24'>
-        <div className='flex flex-col justify-center md:w-1/3 items-center p-8 rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
+    <div className='flex flex-col gap-4 md:flex-row justify-around md:p-8 md:mx-24'>
+        <div className='flex flex-col justify-center md:w-1/3 items-center p-8 md:rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
           <h1 className='text-4xl'>Basic</h1>
           <h1 className='text-6xl'>R 600</h1>
           <p>Per Month</p>
@@ -16,7 +16,7 @@ const PriceCards = () => {
           </ul>
           <button className='p-2 m-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]'>Button</button>
         </div>
-        <div className='flex flex-col justify-center items-center p-8 rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
+        <div className='flex flex-col justify-center items-center p-8 md:rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
           <h1 className='text-4xl'>Standard</h1>
           <h1 className='text-6xl'>R 2 400</h1>
           <p>Per Month</p>
@@ -29,7 +29,7 @@ const PriceCards = () => {
           </ul>
           <button className='p-2 m-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]'>Button</button>
         </div>
-        <div className='flex flex-col justify-center items-center p-8 rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
+        <div className='flex flex-col justify-center items-center p-8 md:rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
           <h1 className='text-4xl'>Premium</h1>
           <h1 className='text-6xl'>R 9 600</h1>
           <p>Per Month</p>
