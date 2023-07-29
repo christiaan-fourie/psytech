5455d3d5b2c8b8da5290513db328197a92a7691b
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-07-29 02:21:22 +0200

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,28 +1,20 @@
-'use client'
 import Image from 'next/image'
 
-import MatrixBackgroundComponent from "./MatricBackgroundComponent"
-
-
 export default function Hero() {
-
-
-
   return (
-    <div>
-        <div className='flex items-center pt-[160px]'>
-          <div className='flex flex-col md:flex-row mx-12 justify-center items-center'>
-            <Image alt='Psytech Software Core' className='animate-pulse' width={500} height={250} src='/technology.png'></Image>
+    <div className='px-16 md:px-10 my-6'>
+        <div className='flex justify-center pt-24'>
+          <div className='flex flex-col md:flex-row items-center animate-pulse'>
+            <Image alt='Psytech Software Core' width={400} height={200} src='/authenticate.png'></Image>
             <div>
-              <h1 className='text-4xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-blue-700 to-red-500'>Security is at our Core</h1>
-              <p>
-                Immerse yourself in the rich tapestry of magic and technology, and let the tale of extraordinary adventure awaken the dreamer and inventor within you. Step into this fantastical realm where anything is possible, and let the wonders of imagination take flight!
+              <h1 className='text-6xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Authenticity is at our Core</h1>
+              <p className='font-sans font-light text-lg'>
+                At Psytech, We understand the importance of trust and transparency in the digital world. <br /> Therefore, we strive to create software solutions that are genuine, reliable, and true to our values. <br /> Resulting in a commitment to authenticity is reflected in everything we do.
               </p>
+              <button className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4'>Explore</button>
             </div>
           </div>
         </div>
-        <MatrixBackgroundComponent />
-    </div>
-    
+    </div>    
   )
 }
