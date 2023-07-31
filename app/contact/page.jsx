112bdfa38b532a1c866667de0dd80283359e7e5f
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-07-31 09:09:32 +0200

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -12,8 +12,8 @@ export default function page() {
         
 
         <div className='flex flex-col md:flex-row justify-center items-center min-h-screen'>
-            <div className='flex flex-col justify-center items-center p-8'>
-                <div className='p-24 text-center z-0'>
+            <div className='flex flex-col justify-center items-center'>
+                <div className=' text-center z-0'>
                     <h4>Get in Touch</h4>
                     <h1 className='text-6xl'>We are <span className='text-blue-500'>available</span></h1>
                     {/* Social Icons Using FA */}
@@ -24,17 +24,15 @@ export default function page() {
                         <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.linkedin.com/" target='_blank'> <FaLinkedin size={30} /> </Link>
                     </div>
                 </div>
-            </div>
-            <div className='flex justify-center items-center p-8 rounded-3xl shadow-[0_0_16px_5px_#2daa5255] backdrop-blur-sm'>
-                <form action="" className='flex flex-col'>
-                    <input type="text" placeholder='Name' className='p-2 m-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]' />
-                    <input type="email" placeholder='Email' className='p-2 m-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]' />
-                    <textarea name="" id="" cols="30" rows="10" placeholder='Message' className='p-2 m-2  rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]'></textarea>
-                    <button className='p-2 m-2 rounded-md focus:outline-none focus:ring-2 focus:ring-[#2daa52]'>Send</button>
-
-                </form>
-            </div>
+            </div>            
         </div>
+        <div className='flex flex-col md:flex-row py-24 items-center justify-center gap-6'>
+            <div className='text-6xl font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-[#517dbf] to-[#2daa52]'>Subscribe</div>
+            <div className='flex flex-col md:flex-row items-center gap-4'>
+            <input className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4 w-[40vw]' placeholder='Email Address' />
+            <button className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4'>Subscribe</button>
+            </div>
+        </div>   
         <ParticleSystem />
     </div>
   )
