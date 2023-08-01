8d5d993e0030f64f77c30a52bd46cf7b4a0d5e0f
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-02 01:56:14 +0200

    Create page.jsx

---
+++ app/company/careers/page.jsx
@@ -0,0 +1,65 @@
+import Header from '@/components/Header/Header'
+import React from 'react'
+
+const Careers = () => {
+  return (
+    <div>
+      <Header />
+        <div className='pt-24'> 
+            <div className=''>
+                <div className='flex h-screen justify-center'>
+                    <div className='flex flex-col md:flex-row items-center animate-pulse'>
+                        <div>
+                            <h1 className='text-6xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Careers</h1>
+                            <p className='font-sans font-light text-lg'>
+                                At Psytech, We understand the importance of trust and transparency. <br /> Therefore, we strive to assemble the most ambitious teams to create <br />software solutions that are genuine, reliable, and true to our values. <br /> Resulting in a commitment to authenticity is reflected in everything we do.
+                            </p>
+                            <button className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4'>Get Onboard</button>
+                        </div>
+                    </div>
+                    
+                </div>
+                {/* <h1 className='text-6xl text-center py-6 border-t border-green-700 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Culture</h1> */}
+                {/* Our Culture */}
+                {/* <div className='flex justify-center p-12 bg-green-300'>
+                    <div className='flex flex-col md:flex-row items-center'>
+                        <div className='text-center'>
+                            
+                            <div className='flex flex-col md:flex-row gap-4'>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Collaboration</h1>
+                                    <p>Encouraging teamwork and open communication among employees fosters creativity and problem-solving.</p>
+                                </div>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Innovation</h1>
+                                    <p>Nurturing a culture that values and rewards creativity and innovative thinking can lead to groundbreaking solutions.</p>
+                                </div>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Continuous Learning</h1>
+                                    <p>Supporting ongoing professional development and learning opportunities helps employees stay updated with the latest trends and technologies.</p>
+                                </div>                                
+                            </div>
+                            <div className='flex flex-col md:flex-row gap-4 mt-4'>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Diversity & Inclusion</h1>
+                                    <p>Embracing diversity and creating an inclusive environment leads to a richer mix of perspectives, which can enhance creativity and productivity.</p>
+                                </div>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Work-Life Balance</h1>
+                                    <p>Promoting a healthy work-life balance ensures employee well-being and helps maintain long-term productivity.</p>
+                                </div>
+                                <div className='bg-neutral-950 p-4'>
+                                    <h1 className='text-4xl my-4 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Agile Mindset</h1>
+                                    <p>Adopting an agile mindset allows the company to adapt quickly to changing market demands and deliver high-quality products efficiently.</p>
+                                </div>                                
+                            </div>
+                        </div>
+                    </div>
+                </div> */}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Careers
