f1492a01cdf2ec70b459080e44b0b012ab1b897f
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-02 01:56:22 +0200

    Update page.jsx

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -2,7 +2,7 @@ import Header from '@/components/Header/Header'
 import React from 'react'
 import Link from 'next/link'
 
-import { FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import { FaFacebook, FaTwitter, FaWhatsapp, FaLinkedin } from 'react-icons/fa'
 import ParticleSystem from '@/components/ParticleSystem/ParticleSystem'
 
 export default function page() {
@@ -18,10 +18,10 @@ export default function page() {
                     <h1 className='text-6xl'>We are <span className='text-blue-500'>available</span></h1>
                     {/* Social Icons Using FA */}
                     <div className='flex justify-center items-center space-x-4 mt-8'>
-                        <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.facebook.com/" target='_blank'> <FaFacebook size={30} /> </Link>
-                        <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.twitter.com/" target='_blank'> <FaTwitter size={30}  /> </Link>
-                        <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.instagram.com/" target='_blank'> <FaInstagram  size={30} /> </Link>
+                        <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.facebook.com/psytechstudio" target='_blank'> <FaFacebook size={30} /> </Link>
                         <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://www.linkedin.com/" target='_blank'> <FaLinkedin size={30} /> </Link>
+                        {/* Whatsapp */}
+                        <Link className='hover:bg-[#2daa52] rounded-full p-1 transition-all duration-500' href="https://wa.me/0718737005" target='_blank'> <FaWhatsapp size={30} /> </Link>
                     </div>
                 </div>
             </div>            
