564b8fbedb178f696d4a81ce179dc4cb5796eb6a
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-02 23:20:48 +0200

    Create page.jsx

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,163 @@
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+import React from 'react'
+
+const privacyPolicy = () => {
+  return (
+    <div>
+        <Header />
+        <div className='py-24 text w-1/2 mx-auto'>
+            <h1 className='text-4xl text-center font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Privacy Policy for Psytech Studio</h1>
+
+
+            <p className='text-sm'>
+                At Psytech Studio, we are committed to safeguarding your personal information and ensuring your privacy. This privacy policy outlines how we collect, use, protect, and manage your personal data. Please read this policy carefully to understand how we handle your personal information when you interact with our website, applications, and services.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Personal Information Collection</h3>
+
+            <p className='text-sm'>
+                When you visit our website or use our applications, we may collect personal information that you provide voluntarily. This information may include your name, email address, mailing address, phone number, or other details that help us provide you with a better experience.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Information Collection Timing</h3>
+
+            <h5 className='text-md text-start font-bold my-2 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>We gather information when you:</h5>
+
+            <ul className='text-sm list-disc list-inside'>
+                <li>Register on our website</li>
+                <li>Place an order for our products or services</li>
+                <li>Subscribe to our newsletter</li>
+                <li>Participate in surveys or respond to forms</li>
+                <li>Interact with our website in any other way</li>
+            </ul>
+
+            <p className='text-sm mt-4'>
+                Additionally, we utilize tools to track website usage, capturing your IP address and country for analytical purposes.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Use of Your Information</h3>
+
+            <h5 className='text-md text-start font-bold my-2 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>We utilize the information collected from you to:</h5>
+
+            <ul className='text-sm list-disc list-inside'>
+                <li>Personalize your user experience and tailor content to your interests</li>
+                <li>Enhance and optimize our website</li>
+                <li>Improve customer service by responding effectively to your requests</li>
+                <li>Administer contests, promotions, surveys, or other site features</li>
+                <li>Process transactions efficiently</li>
+                <li>Send emails regarding orders, products, or services</li>
+                <li>Adhere to the General Data Protection Regulation (GDPR) when processing personal data</li>
+            </ul>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Retention of Personal Data</h3>
+
+            <p className='text-sm'>
+                Personal data collected through our website is retained indefinitely, ensuring compliance with GDPR regulations.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Security Measures</h3>
+
+            <p className='text-sm'>
+                Your security is paramount to us. We employ various security measures to protect your personal information. Our website undergoes regular security scans for vulnerabilities and malware. Access to your personal information is restricted to authorized personnel only, and all sensitive data is encrypted using Secure Socket Layer (SSL) technology.
+            </p>
+
+            <p className='text-sm'>
+                We process transactions through a trusted gateway provider, ensuring that no sensitive information is stored or processed on our servers.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Use of Cookies</h3>
+
+            <p className='text-sm'>
+                Cookies are used to enhance your browsing experience. They help us remember your preferences, offer better site experiences, and analyze site traffic. By using cookies, we aim to improve our services and provide you with a seamless experience.
+            </p>
+
+            <p className='text-sm'>
+                You can adjust your cookie preferences through your browser settings, but please note that disabling cookies might affect some website features.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Third-Party Disclosure</h3>
+
+            <p className='text-sm'>
+                We do not share your personally identifiable information with external parties, except for website hosting partners and service providers assisting us in operating our website and serving you better. Your information is kept confidential, and we may only disclose it when required by law or to protect our rights, property, or safety.
+            </p>
+
+            <p className='text-sm'>
+                Non-personally identifiable information may be provided to third parties for marketing, advertising, or other uses.
+            </p>
+
+            <p className='text-sm'>
+                Third-party links on our website are governed by their own privacy policies. We are not responsible for the content or activities of these linked sites.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Third-Party Links</h3>
+
+            <p className='text-sm'>
+                Our website does not directly promote or offer third-party products or services.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Rights and Regulations</h3>
+
+            <ul className='text-sm list-disc list-inside'>
+                <li>We comply with the South African Protection of Information Act, 2013 (POPI Act).</li>
+                <li>We respect your right to access, correct, block, delete, or restrict processing of your personal data as per the GDPR.</li>
+                <li>You can visit our site anonymously.</li>
+                <li>Our privacy policy is easily accessible from our homepage.</li>
+                <li>You&apos;ll be notified of any privacy policy changes.</li>
+                <li>You can change your personal information by contacting us through email or phone.</li>
+            </ul>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Do-Not-Track Signals and COPPA</h3>
+
+            <p className='text-sm'>
+                We honor Do-Not-Track signals, and our site is not designed to track, plant cookies, or engage in behavioral tracking. We do not specifically target children under 13.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Fair Information Practices</h3>
+
+            <p className='text-sm'>
+                We adhere to Fair Information Practices principles and will notify you within 7 business days via email or in-site notification in the event of a data breach.
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>CAN-SPAM Act</h3>
+
+            <p className='text-sm'>
+                We comply with the CAN-SPAM Act, and you can unsubscribe from receiving future emails at any time by contacting us through email or phone.
+
+            </p>
+
+            <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Contact Us</h3>
+
+            <p className='text-sm'>
+                If you have any questions regarding our privacy policy, please contact us at:
+            </p>
+
+            <p className='text-sm'>
+                <span className='font-bold'>Address:</span> 1st Floor, 1 Park Lane, Century City, Cape Town, 7441
+            </p>
+
+            <p className='text-sm'>
+                <span className='font-bold'>Email:</span> hello[at]psytechgroup[dot]co[dot]za
+            </p>
+
+            <p className='text-sm'>
+                <span className='font-bold'>Registration Number:</span> 2021 / 398282 / 07
+            </p>
+                
+            <p className='text-sm py-2'>
+                    Thank you for entrusting us with your personal information. 
+            </p>
+            <p className='text-sm py-2'>
+                Your privacy and security are essential to us at Psytech Studio. We are committed to protecting your personal information and your right to privacy. 
+            </p>
+            <p className='text-sm py-2'>
+                If you have any questions or concerns about our policy, or our practices with regards to your personal information, please contact us!
+            </p>
+
+        </div>
+        <Footer />
+    </div>
+  )
+}
+
+export default privacyPolicy
