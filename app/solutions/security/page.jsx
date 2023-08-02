48e1c10728df4a28815e8e3d7f68f8fb0e1f0e12
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-02 11:23:01 +0200

    Update page.jsx

--- app/solutions/security/page.jsx
+++ app/solutions/security/page.jsx
@@ -1,6 +1,7 @@
 import Header from '@/components/Header/Header'
 import React from 'react'
 import Image from 'next/image'
+import Footer from '@/components/Footer/Footer'
 
 const Security = () => {
   return (
@@ -31,9 +32,9 @@ const Security = () => {
         <div className='flex flex-col gap-4 lg:w-2/3'>
             <Image alt='Psytech Software Core' width={1200} height={1200} src='/dashboardSecurityPreview.jpg' className='rounded-lg glass w-full opacity-90'></Image>
             <Image alt='Psytech Software Core' width={1200} height={1200} src='/codeWallCropped.jpg' className='hidden xl:block left-0 top-0 w-screen bg-cover -z-50 fixed'></Image>
-        </div>
-        
-      </div>      
+        </div>        
+      </div>
+      {/* <Footer />       */}
     </div>
   )
 }
