a05b579ab21b9060ceea44cff90d2c7aa49382d5
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-02 23:20:51 +0200

    Create page.jsx

---
+++ app/terms-and-conditions/page.jsx
@@ -0,0 +1,101 @@
+import Footer from '@/components/Footer/Footer';
+import Header from '@/components/Header/Header';
+import React from 'react';
+
+const termsAndConditions = () => {
+  return (
+    <div>
+      <Header />
+      <div className='py-24 text w-1/2 mx-auto'>
+        <h1 className='text-4xl text-center font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          Terms and Conditions for Psytech Studio
+        </h1>
+
+        <p className='text-sm'>
+          Welcome to the Psytech Studio website “the Website,” an online resource owned and controlled by Psytech Studio and/or its affiliated and related companies “Psytech Studio.” Your use of this website is subject to the terms and conditions set out in these Terms of Use. If you do not agree to these Terms of Use, you may not use or access the Website. Psytech Studio reserves the right to modify the content on the Website at any time as well as to modify these Terms of Use. By continuing to use or access the Website, you indicate your acceptance of these terms and agree to be bound by any such modified Terms of Use. If you do not agree to any new or modified Terms of Use, you may not continue using the Website.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          1. Use of Content on the Website
+        </h3>
+
+        <p className='text-sm'>
+          You may view, download, print, or display contents from the Website provided that your use is for information purposes of a personal and non-commercial nature only and provided that you retain all copyright and other notices found in the original content. You may not modify or alter the contents of the Website in any way, or reproduce, republish, perform, distribute, or display the contents of this Website for any public or commercial purpose. You may not use the contents of the Website to prepare any derivative work. You may not frame, enclose, or extract any trademark, image, text, form, or other proprietary information on the Website without written permission from Psytech Studio. You may not use the word “Psytech Studio” or a derivative thereof as a meta tag or hidden text without written permission from Psytech Studio, nor may you use Psytech Studio’s logo or other trademarks as part of any link. Any use of the contents of the Website on any other website is prohibited. Use of the website does not entitle you to any rights of ownership in the content, and you expressly acknowledge that Psytech Studio retains all its rights in the Website. Should your use of the Website not be in accordance with these Terms of Use, you must immediately cease using the Website, and you must destroy any downloaded or printed material.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          2. SUBMITTING INFORMATION TO THE WEBSITE
+        </h3>
+
+        <p className='text-sm'>
+          Psytech Studio may provide an interface for users to post comments, discussions, or other content on the Website. Psytech Studio does not want to receive confidential or proprietary information from you through the Website. Therefore, any information you submit or post to the Website will be considered non-confidential and nonproprietary. You must not submit information that is obscene, illegal, threatening, or defamatory, or submit comments that infringe the privacy or intellectual property rights of a third party. You may not submit any form of spam, virus, or mass mail to the Website, and you may not provide misleading information as to your identity. By submitting any information, you expressly grant to Psytech Studio a non-exclusive, royalty-free, perpetual, irrevocable, and sublicensable right to reproduce, use, adapt, publish, modify, and distribute such content anywhere in the world. You warrant that the information you submit is accurate and that you have the permission to submit it.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          3. INDEMNITY
+        </h3>
+
+        <p className='text-sm'>
+          You agree to defend, indemnify, and hold Psytech Studio harmless against any claims, expenses, liabilities, and losses resulting directly or indirectly from your use of the Website or from any content you may supply.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          4. THIRD PARTY WEBSITES
+        </h3>
+
+        <p className='text-sm'>
+          Psytech Studio may, at its discretion, choose to link to third-party websites or content. Such links do not constitute an endorsement of such third-party website, nor is Psytech Studio responsible for any viruses, content, damages, or disputes resulting from your access to such third-party websites.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          5. DISCLAIMER
+        </h3>
+
+        <p className='text-sm'>
+          Except as expressly stated otherwise in an agreement between you and Psytech Studio, all content, services, products, and software provided on this website are provided “as is” and without warranty of any kind, either express or implied. Psytech Studio disclaims all warranties, express or implied, including, without limitation, the warranties of merchantability, fitness for a particular purpose, and non-infringement. Psytech Studio does not warrant that the website, its content, or the products and services it offers will meet your requirements or be suitable for your purposes. Unless agreed otherwise in any agreement between you and Psytech Studio, Psytech Studio shall not be liable for any direct, indirect, special, consequential, incidental, or punitive damages, loss of profits or revenue, or interruption of business arising in any way from your use of the Psytech Studio website or Psytech Studio&apos;s services or products, even if Psytech Studio had been advised of the possibility of such damages.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          6. PRESERVATION OF RIGHTS
+        </h3>
+
+        <p className='text-sm'>
+          Except as expressly provided herein, no other rights are granted by Psytech Studio under this Agreement. All rights not expressly granted hereunder are reserved to Psytech Studio.
+        </p>
+
+        <h3 className='text-xl text-start font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+          7. GENERAL LEGAL PROVISIONS
+        </h3>
+
+        <p className='text-sm'>
+          In the event any provision in these Terms of Use is found to be invalid, illegal, or unenforceable, the validity, legality, and enforceability of any of the remaining provisions shall not in any way be affected or impaired. These Terms of Use shall be governed and controlled by the laws of the Republic of South Africa, and you and Psytech Studio both agree to the sole jurisdiction of the Western Cape High Court, Cape Town, in relation to any dispute arising out of this agreement.
+        </p>
+
+        {/* Other sections of the terms and conditions can be similarly structured */}
+
+        <p className='text-sm py-2'>
+          If you have any questions or concerns about these terms and conditions, please contact us at:
+        </p>
+
+        <p className='text-sm'>
+          <span className='font-bold'>Address:</span> 1st Floor, 1 Park Lane, Century City, Cape Town, 7441
+        </p>
+
+        <p className='text-sm'>
+          <span className='font-bold'>Email:</span> hello[at]psytechgroup[dot]co[dot]za
+        </p>
+
+        <p className='text-sm'>
+          <span className='font-bold'>Registration Number:</span> 2021 / 398282 / 07
+        </p>
+
+        <p className='text-sm py-2'>
+          Thank you for reading and agreeing to our terms and conditions. Your continued use of Psytech Studio&apos;s services and website signifies your acceptance of these terms.
+        </p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default termsAndConditions;
