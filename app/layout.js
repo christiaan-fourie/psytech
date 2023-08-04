90d37d31d9b99c0554362cd3577def1c7bf926dd
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-04 10:00:52 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,19 +4,24 @@ import { Inter } from 'next/font/google'
 import StyledCursor from '@/components/Cursor/Cursor'
 import ScrollToTop from '@/components/ScrollToTop/ScrollToTop'
 
+import Footer from '@/components/Footer/Footer'
+import Header from '@/components/Header/Header'
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Psytech Studio',
-  description: 'We are a Software Developers.',
+  title: 'Welcome to Psytech Studio',
+  description: 'We are a team of passionate designers and developers who love to create awesome websites and apps.',
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <StyledCursor />
+      <body className={inter.className}>        
+        <Header />
         {children}
+        <Footer />
+        <StyledCursor />
         <ScrollToTop />
       </body>
     </html>
