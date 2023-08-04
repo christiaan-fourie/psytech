f2dfda75da0463e61ab7600fed0d30672dc63e04
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-04 18:37:44 +0200

    Update page.jsx

--- app/company/about/page.jsx
+++ app/company/about/page.jsx
@@ -1,6 +1,9 @@
 'use client'
 
 import Image from "next/image"
+import Link from "next/link"
+
+import { FaKey, FaHome } from "react-icons/fa"
 
 import { useState, useEffect } from "react"
 
@@ -36,69 +39,63 @@ const About = () => {
       }, 1000)
     }
   }, [isTyping])
+
+  // Get the description of the card
+  function getCardDescription(item) {
+    const descriptions = {
+      Security:
+        'Protecting against unauthorized access and damage through measures like encryption, firewalls, and access controls.',
+      Privacy:
+        'Ensuring personal information remains confidential and away from public view, safeguarding your privacy.',
+      Reliability:
+        'Guaranteeing we and our systems will perform consistently without failure over time, ensuring an uptime of 99%.',
+    };
+  
+    return descriptions[item];
+  }  
   
 
   return (
-    <div>
-          <div className='flex flex-col lg:flex-row justify-between items-center pt-24 px-12'>
+    <section>
+          {/* Section for About Us */}
+          <div className='flex flex-col lg:flex-row justify-between items-center pt-24 px-12 h-screen'>
             <div>
               <h1 className='text-4xl text-left font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>{ headerText }</h1>
               {
                 !isTyping && (
-                  <div className="flex gap-2">
-                    <button className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4'>Learn More</button>
-                    <button className='bg-[#2daa52]  my-4 py-2 px-4'>See offers</button>
+                  <div className="flex gap-2 h-12">
+                    <Link href='/' className='rounded-l-full hover:bg-[#2daa5200] text-neutral-100 hover:text-[#2daa52] border glassLight border-[#2daa52] px-4 flex justify-end items-center gap-2 hover:gap-4 transition-all'>Learn More<FaHome /></Link>
+                    <Link href='/solutions/packages' className='rounded-r-full bg-[#2daa52] hover:bg-[#2daa5200] text-neutral-950 hover:text-[#2daa52] border glassLight border-[#2daa52] px-4 flex items-center gap-2 hover:gap-4 transition-all'><FaKey />See Offers</Link>
                   </div>                  
                 )
               }
             </div>
             <Image alt='Psytech Software Devices' className="animate-pulse"  width={600} height={400} src={image}></Image>
-          </div>   
-          <div className="flex-col justify-center items-center my-12 py-12 glassLightNoBd">
-                <h1 className='text-4xl text-center font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Our Mission</h1>
-                <p className='font-sans font-light text-lg text-center'>
-                We connect people to your business, this ensures that you are able to make informed decisions and provide the best service to your customers. <br />
-                  <button className='border border-[#2daa52] text-[#2daa52] my-4 py-2 px-4'>Explore More</button>
-                </p>
-                <div className="p-24 gap-4 bg-neutral-900">
-                  <h1 className='text-4xl text-center font-bold my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Our tech stack.</h1>
-                  <div className="flex flex-col gap-4 md:flex-row justify-around items-center pt-24 px-12">
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/react.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse invert"  width={techStachSize} height={techStachSize} src="/next.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/tailwind.webp"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/node.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/express.png"></Image>
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-4 md:flex-row justify-around items-center pt-24 px-12">
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/python.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/Tux.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/mongodb.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/googlecloud.png"></Image>
-                    </div>
-                    <div className="flex flex-col items-center">
-                      <Image alt='Psytech Software Devices' className="animate-pulse"  width={techStachSize} height={techStachSize} src="/postman.png"></Image>
-                    </div>
-                  </div>
-                </div>                     
-              </div>       
-    </div>
+          </div>  
+
+
+          {/* Three Cards that display the values */}
+          <div className='flex-col justify-center items-center my-12 py-12 glassLightNoBd text-center'>
+            <h1 className='text-4xl my-6 bg-clip-text font-bold text-transparent bg-gradient-to-r from-green-700 to-blue-500'>
+              We Value
+            </h1>
+            <div className='flex flex-col sm:flex-row justify-center items-center gap-12'>
+              {['Security', 'Privacy', 'Reliability'].map((item, index) => (
+                <div
+                  key={index}
+                  className='w-4/5 md:w-1/5 bg-neutral-900 rounded-lg shadow-lg p-4'
+                >
+                  <h2 className='text-2xl font-bold py-2'>{item}</h2>
+                  <p className='font-sans font-light text-lg bg-opacity-50'>
+                    {getCardDescription(item)}
+                  </p>
+                </div>
+              ))}
+            </div>
+          </div>
+
+                
+    </section>
   )
 }
 
