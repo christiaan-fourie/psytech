eeb510e46c281b0e96e51abf9ae0b2623b843cef
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-04 10:01:05 +0200

    Update page.jsx

--- app/solutions/packages/page.jsx
+++ app/solutions/packages/page.jsx
@@ -1,16 +1,12 @@
-import Footer from '@/components/Footer/Footer'
-import Header from '@/components/Header/Header'
 import PriceCards from '@/components/PriceCards/PriceCards'
 import React from 'react'
 
 const Packages = () => {
   return (
     <div>
-        <Header />
         <div className='pt-24'>
             <PriceCards />
-        </div> 
-        <Footer />       
+        </div>      
     </div>    
   )
 }
