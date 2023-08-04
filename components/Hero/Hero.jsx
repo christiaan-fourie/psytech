698abe2de2ffc1c954aed2b3a7e347c4be5b4e7c
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-04 18:38:02 +0200

    Update Hero.jsx

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,21 +1,23 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import { FaKey, FaHome } from 'react-icons/fa'
 
 export default function Hero() {
   return (
-    <div className='px-16 md:px-10 my-6'>
-        <div className='flex justify-center pt-24'>
-          <div className='flex flex-col md:flex-row items-center animate-pulse'>
-            <Image alt='Psytech Software Core' width={400} height={200} src='/authenticate.png'></Image>
-            <div>
-              <h1 className='text-6xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Authenticity is at our Core</h1>
-              <p className='font-sans font-light text-lg'>
-                At Psytech, We understand the importance of trust and transparency in the digital world. <br /> Therefore, we strive to create software solutions that are genuine, reliable, and true to our values. <br /> Resulting in a commitment to authenticity is reflected in everything we do.
-              </p>
-              <Link href='/company/about' className='border border-[#2daa52] text-[#2daa52] my-12 py-2 px-4'>Explore</Link>
-            </div>
-          </div>
+    <div className='flex flex-col md:flex-row px-12 pt-24 sm:pt-6 min-h-screen items-center sm:gap-4'>
+      <div  className='w-1/2 md:w-1/2 lg:w-1/3'>
+        <Image alt='Psytech Software Core' width={400} height={200} src='/authenticate.png'></Image>
+      </div>
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-4xl md:text-6xl my-6 bg-clip-text text-transparent bg-gradient-to-r from-green-700 to-blue-500'>Privacy & Simplicity</h1>
+        <p className='font-sans font-light md:text-lg'>
+          At Psytech, We strive to create software solutions that are genuine, reliable, and true to our values. <br /> Resulting in a commitment to our customers and their privacy. <br /> We are a team of passionate individuals who are dedicated to creating a sustainable future for all. <br />
+        </p>
+        <div className="flex gap-2 h-12">
+          <Link href='/company/about' className='rounded-l-full hover:bg-[#2daa5200] text-neutral-100 hover:text-[#2daa52] border glassLight border-[#2daa52] px-4 flex justify-end items-center gap-2 hover:gap-4 transition-all'>About Us<FaHome /></Link>
+          <Link href='/solutions/packages' className='rounded-r-full bg-[#2daa52] hover:bg-[#2daa5200] text-neutral-950 hover:text-[#2daa52] border glassLight border-[#2daa52] px-4 flex items-center gap-2 hover:gap-4 transition-all'><FaKey />See Offers</Link>
         </div>
-    </div>    
+      </div>
+    </div>
   )
 }
