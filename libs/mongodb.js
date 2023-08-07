391f6bc1f21ba92ca01dab33d331780c9621bc12
Christiaan Fourie <christiaanfourie60@gmail.com>
2023-08-07 09:39:52 +0200

    Update mongodb.js

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
-
-    // Define URI as string
-    const uri = String(process.env.MONGODB_URI);
-
-  try {
-    await mongoose.connect(uri);
-    console.log("Connected to MongoDB.");
-  } catch (error) {
-    console.log(error);
-  }
+    console.log(process.env.MONGODB_URI);
+    
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to MongoDB.");
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 export default connectMongoDB;
